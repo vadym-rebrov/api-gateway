03eeed170a14d30cfe25d1b9cff725cafa8f08b7
Vadym <vadym.rebrov@gmail.com>
2026-01-27 12:58:43 +0200

    Initial commit

---
+++ src/main/java/dev/profitsoft/internship/rebrov/apigateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package dev.profitsoft.internship.rebrov.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/login/**", "/error").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
