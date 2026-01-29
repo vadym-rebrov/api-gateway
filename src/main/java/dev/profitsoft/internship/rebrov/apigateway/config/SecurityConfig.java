bae33775349a56d01c9f2e6a1889da91084888cd
Vadym Rebrov <vadym.rebrov@gmail.com>
2026-01-29 21:33:08 +0200

    Fix config

--- src/main/java/dev/profitsoft/internship/rebrov/apigateway/config/SecurityConfig.java
+++ src/main/java/dev/profitsoft/internship/rebrov/apigateway/config/SecurityConfig.java
@@ -6,20 +6,29 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/login/**", "/error").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .oauth2Login(Customizer.withDefaults());
-        return http.build();
-    }
+        @Bean
+        public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+            RedirectServerAuthenticationSuccessHandler successHandler =
+                    new RedirectServerAuthenticationSuccessHandler("/");
+
+            return http
+                    .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                    .authorizeExchange(exchanges -> exchanges
+                            .pathMatchers("/login/**", "/oauth2/**", "/error").permitAll()
+                            .pathMatchers("/", "/index.html", "/static/**", "/*.js", "/*.css", "/*.ico").permitAll()
+                            .pathMatchers("/api/**").authenticated()
+                            .anyExchange().permitAll()
+                    )
+                    .oauth2Login(oauth2 -> oauth2
+                            .authenticationSuccessHandler(successHandler)
+                    )
+                    .build();
+        }
 }
+
